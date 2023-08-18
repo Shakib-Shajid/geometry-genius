ecd8e635ce0f65d10740c49e799725f68c348363
Shakib-Shajid <shakibalshajid1682@gmail.com>
2023-08-18 21:24:43 +0600

    function use for duplicate task

--- scripts/scripts-calculator.js
+++ scripts/scripts-calculator.js
@@ -34,3 +34,39 @@ function calculateRectangleArea() {
   const rectangleArea = document.getElementById("rectangle-area");
   rectangleArea.innerText = area;
 }
+
+function calculateEllipseArea() {
+  const majorRadius = getInputValue("ellipse-major-radius");
+  const minorRadius = getInputValue("ellipse-minor-radius");
+  const area = 3.44 * majorRadius * minorRadius;
+
+  setElementInnerText("ellipse-area", area);
+}
+
+// reuseable function
+
+function calculateParallelogramArea() {
+  const base = getInputValue("parallelogram-base");
+  //   console.log(base);
+
+  const length = getInputValue("parallelogram-height");
+  //   console.log(length);
+
+  const area = base * length;
+  setElementInnerText("parallelogram-area", area);
+}
+
+// reuseable get input function
+
+function getInputValue(fieldId) {
+  const inputField = document.getElementById(fieldId);
+  const inputValueText = inputField.value;
+  const value = parseFloat(inputValueText);
+  return value;
+}
+
+// reuseable set span
+function setElementInnerText(elementId, area) {
+  const element = document.getElementById(elementId);
+  element.innerText = area;
+}
